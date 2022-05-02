5d024fe7aac0ae9d9b81a46f8035145f9fe92e62
Navya40 <102596854+Navya40@users.noreply.github.com>
2022-05-02 14:03:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+x = 0;
+y = 0;
+screen_width = 0;
+screen_height = 0;
+draw_apple = "";
+apple = "";
+speak_data = "";
+to_number = "";
+
+function preload() {
+    apple = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxRerNTh0gfQD6Tmixs9sThbpps66aXBPJoA&usqp=CAU');
+}
+
+function recognition.onresult() {
+    to_number = Number(content);
+}
+
+Number.isInteger() {
+    if (Number.isInteger(to_number))
+}
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("status").innerHTML = "System is listing please speak";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var content = event.results[0][0].transpript;
+
+    document.getElementById("status").innerHTML = "The Speech has beed recognized as: " + content;
+    if (content == "circle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started drawing circle ";
+        draw_circle = "set";
+    }
+    if(content =="rectangle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Startinf drawing rectangle ";
+        draw_rect = "set";
+    }
+}
+function setup() {
+    canvas = createCanvas(900, 600);
+}
+
+function draw() {
+    if(draw_circle == "set")
+    {
+        radius = Math.floor(Math.random() * 100);
+        circle(x,y,radius);
+        document.getElementById("status").innerHTML = "Circle is drawn. ";
+        draw_circle = "";
+    }
+
+    if(draw_rect == "set")
+    {
+        rect(x,y,70,50);
+        document.getElementById("status").innerHTML = "Rectangle is drawn. ";
+        draw_rect = "";
+    }
+}
